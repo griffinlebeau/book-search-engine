f5aa11c67dfa3375fd493febdc6d79fd968c73b0
Griffin <griffinlebeau2@gmail.com>
2022-10-06 16:52:10 -0500

    back end tasks done

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,19 +6,16 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req, res }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
-
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }
-
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
@@ -29,7 +26,7 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
